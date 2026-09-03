35194200fd6b3433da1501d6cb412b545984d091
Nebojsa <horvatnebojsa@yahoo.com>
2026-09-03 15:23:01 +0200

    Add: k-closest-points-to-origin - submission-1

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-1.go
@@ -0,0 +1,75 @@
+type MinHeap struct{
+	data [][]int
+	k int
+}
+
+func (h *MinHeap) Len ()int { return len(h.data)}
+func (h *MinHeap) Push(val []int){
+	h.data = append(h.data, val)
+	h.siftUp(h.Len()-1)
+}
+
+func(h *MinHeap) Pop() []int{
+	top := h.data[0]
+	h.data[0] = h.data[h.Len()-1]
+	h.data = h.data[:h.Len()-1]
+	if h.Len() > 0 {
+		h.siftDown(0)
+	}
+	return top
+}
+
+func (h *MinHeap) siftUp (i int){
+	for i > 0 {
+		parent := (i-1)/2
+		if h.data[parent][2] <= h.data[i][2]{
+			break
+		}
+		h.data[parent], h.data[i] = h.data[i], h.data[parent]
+		i = parent
+	}
+}
+
+func (h *MinHeap) siftDown (i int){
+	n := len(h.data)
+	smallest := i
+	for {
+		left := i*2 +1
+		right := i*2 +2
+		if left < n && h.data[left][2] < h.data[smallest][2]{
+			smallest = left
+		}
+		if right < n && h.data[right][2] < h.data[smallest][2]{
+			smallest = right
+		}
+		if smallest == i{
+			break
+		}
+		h.data[i], h.data[smallest] = h.data[smallest], h.data[i]
+		i = smallest
+	}
+}
+
+func Constructor (data [][]int, k int) MinHeap{
+	minHeap := MinHeap{k:k,}
+	for _, val := range data {
+		minHeap.Push(val)
+	}
+	return minHeap
+}
+
+func kClosest(points [][]int, k int) [][]int {
+	minHeap := Constructor([][]int{},k)
+	for _, point := range points{
+		x := point[0]
+		y := point[1]
+		val := []int{x,y, x*x + y*y}
+		minHeap.Push(val)
+	}
+	ret := [][]int{}
+	for i:=0; i<k; i++{
+		val := minHeap.Pop()
+		ret = append(ret, []int{val[0],val[1]})
+	}
+	return ret
+}
